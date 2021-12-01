48994dad1925683b695ac3d85ab7e358c4794879
Axel Wagner <mero@merovius.de>
2021-12-01 16:54:28 +0100

    Day 1: Make solution a bit more pythonic

--- day01/day01.py
+++ day01/day01.py
@@ -1,7 +1,7 @@
 data = [int(l) for l in open('input.txt', 'r').readlines()]
 #data = [199,200,208,210,200,207,240,269,260,263]
 
-count = sum(1 if w[0] < w[1] else 0 for w in zip(data, data[1:]))
-print("Number of increases:", count)
-count = sum(1 if w[0] < w[1] else 0 for w in zip(data, data[3:]))
-print("Number of windowed increases:", count)
+count = sum(w[0] < w[1] for w in zip(data, data[1:]))
+print(f'Number of increases: {count}')
+count = sum(w[0] < w[1] for w in zip(data, data[3:]))
+print(f'Number of windowed increases: {count}')
