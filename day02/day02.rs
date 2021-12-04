aa50a09de34d1ce56a8a92b11078796e68ab0ce7
Axel Wagner <mero@merovius.de>
2021-12-04 19:24:51 +0100

    Day 2: Make Rust solution more idiomatic

--- day02/day02.rs
+++ day02/day02.rs
@@ -17,45 +17,34 @@ fn main() -> Result<(), Box<dyn Error>> {
 }
 
 enum Op {
-    Forward,
-    Up,
-    Down,
+    Forward(i32),
+    Up(i32),
+    Down(i32),
 }
 
 impl Display for Op {
     fn fmt(&self, f: &mut ::std::fmt::Formatter) -> ::std::result::Result<(), ::std::fmt::Error> {
         match *self {
-            Op::Forward => f.write_str("Forward"),
-            Op::Up => f.write_str("Up"),
-            Op::Down => f.write_str("Down"),
+            Op::Forward(n) => f.write_fmt(format_args!("Forward({})", n)),
+            Op::Up(n) => f.write_fmt(format_args!("Up({})", n)),
+            Op::Down(n) => f.write_fmt(format_args!("Down({})", n)),
         }
     }
 }
 
-struct Command {
-    op: Op,
-    count: i32,
-}
-
-impl Display for Command {
-    fn fmt(&self, f: &mut ::std::fmt::Formatter) -> ::std::result::Result<(), ::std::fmt::Error> {
-        f.write_fmt(format_args!("{}[{}]", self.op, self.count))
-    }
-}
-
-fn parse<R: BufRead>(r: &mut R) -> Result<Vec<Command>, Box<dyn Error>> {
+fn parse<R: BufRead>(r: &mut R) -> Result<Vec<Op>, Box<dyn Error>> {
     let mut prog = Vec::new();
     for line in r.lines() {
         let l = line?;
         let i = l.find(' ').ok_or("no space in line")?;
         let count = l[i + 1..].parse::<i32>()?;
-        let op = match &l[..i] {
-            "forward" => Ok(Op::Forward),
-            "up" => Ok(Op::Up),
-            "down" => Ok(Op::Down),
+        let cmd = match &l[..i] {
+            "forward" => Ok(Op::Forward(count)),
+            "up" => Ok(Op::Up(count)),
+            "down" => Ok(Op::Down(count)),
             _ => Err("invalid command"),
         }?;
-        prog.push(Command { op, count });
+        prog.push(cmd);
     }
     Ok(prog)
 }
@@ -64,6 +53,16 @@ struct Pos {
     horizontal: i32,
     depth: i32,
     aim: i32,
+}
+
+impl Pos {
+    fn new() -> Pos {
+        Pos {
+            horizontal: 0,
+            depth: 0,
+            aim: 0,
+        }
+    }
 }
 
 impl Display for Pos {
@@ -77,33 +76,46 @@ impl Display for Pos {
     }
 }
 
-fn run(prog: &Vec<Command>, apply: fn(&mut Pos, &Command)) -> Pos {
-    let mut p = Pos {
-        horizontal: 0,
-        depth: 0,
-        aim: 0,
-    };
-    for c in prog {
-        apply(&mut p, &c);
+fn run(prog: &Vec<Op>, apply: fn(Pos, &Op) -> Pos) -> Pos {
+    prog.iter().fold(Pos::new(), apply)
+}
+
+fn apply1(p: Pos, cmd: &Op) -> Pos {
+    match cmd {
+        Op::Forward(n) => Pos {
+            horizontal: p.horizontal + n,
+            depth: p.depth,
+            aim: p.aim,
+        },
+        Op::Down(n) => Pos {
+            horizontal: p.horizontal,
+            depth: p.depth + n,
+            aim: p.aim,
+        },
+        Op::Up(n) => Pos {
+            horizontal: p.horizontal,
+            depth: p.depth - n,
+            aim: p.aim,
+        },
     }
-    return p;
 }
 
-fn apply1(p: &mut Pos, c: &Command) {
-    match c.op {
-        Op::Forward => p.horizontal += c.count,
-        Op::Down => p.depth += c.count,
-        Op::Up => p.depth -= c.count,
-    };
-}
-
-fn apply2(p: &mut Pos, c: &Command) {
-    match c.op {
-        Op::Forward => {
-            p.horizontal += c.count;
-            p.depth += c.count * p.aim;
-        }
-        Op::Down => p.aim += c.count,
-        Op::Up => p.aim -= c.count,
+fn apply2(p: Pos, cmd: &Op) -> Pos {
+    match cmd {
+        Op::Forward(n) => Pos {
+            horizontal: p.horizontal + n,
+            depth: p.depth + n * p.aim,
+            aim: p.aim,
+        },
+        Op::Down(n) => Pos {
+            horizontal: p.horizontal,
+            depth: p.depth,
+            aim: p.aim + n,
+        },
+        Op::Up(n) => Pos {
+            horizontal: p.horizontal,
+            depth: p.depth,
+            aim: p.aim - n,
+        },
     }
 }
