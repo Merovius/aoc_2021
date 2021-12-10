be935bb0524ef94fed9908f953c8f5d5bf6470cc
Axel Wagner <mero@merovius.de>
2021-12-10 10:41:53 +0100

    Day 9: Use DFS in Python solution

--- day09/day09.py
+++ day09/day09.py
@@ -6,6 +6,17 @@ def read(name):
 
 data = read('input.txt')
 
+class Point(tuple):
+    def __new__(self, r, c):
+        return tuple.__new__(Point, (r, c))
+
+    def __add__(self, other):
+        return Point(self[0]+other[0], self[1]+other[1])
+
+    @property
+    def val(self):
+        return get(self[0], self[1])
+
 def get(r, c):
     if r < 0 or r >= len(data):
         return 10
@@ -23,34 +34,27 @@ def risk_level(data):
     return rl
 
 def basin_sizes(data):
-    m = defaultdict(lambda: (0, 0))
+    labels = {}
+    n = 0
+    def dfs(p):
+        if p in labels or p.val >= 9:
+            return
+        labels[p] = n
+        for q in [(-1,0), (1,0), (0,-1), (0,1)]:
+            dfs(p+q)
+
     for r in range(len(data)):
         for c in range(len(data[r])):
-            h = get(r, c)
-            if h == 9:
+            p = Point(r, c)
+            if p in labels or p.val >= 9:
                 continue
-            if h > get(r-1, c):
-                m[(r,c)] = (r-1, c)
-            elif h > get(r+1, c):
-                m[(r,c)] = (r+1, c)
-            elif h > get(r, c-1):
-                m[(r,c)] = (r, c-1)
-            elif h > get(r, c+1):
-                m[(r,c)] = (r, c+1)
-            else:
-                m[(r,c)] = (r, c)
-    stable = False
-    while not stable:
-        stable = True
-        for src, dst in m.items():
-            if dst != m[dst]:
-                m[src] = m[dst]
-                stable = False
+            dfs(p)
+            n += 1
 
-    sizes = defaultdict(lambda: 0)
-    for src, dst in m.items():
-        sizes[dst] += 1
-    sizes = sorted(sizes.values())
+    sizes = [0]*n
+    for l in labels.values():
+        sizes[l] += 1
+    sizes.sort()
     return sizes[-1]*sizes[-2]*sizes[-3]
 
 print(f"Risk level: {risk_level(data)}")
