70253b48a97570f2f9784d8968805f6377cde791
Axel Wagner <mero@merovius.de>
2021-12-17 01:20:43 +0100

    Day 15: Use custom 2D array implementation
    In theory, this might be faster because we can index using bitshifts
    only. In practice, this is a micro-optimization without any real impact.

--- day15/day15.rs
+++ day15/day15.rs
@@ -1,7 +1,5 @@
 use std::collections::BinaryHeap;
-use std::ops::{Index,IndexMut};
 
-use array2d::Array2D;
 use simple_error::SimpleError;
 
 type Error = Box<dyn std::error::Error>;
@@ -21,11 +19,11 @@ fn main() -> Result<(), Error> {
     Ok(())
 }
 
-fn parse() -> Array2D<u8> {
+fn parse() -> Grid<u8> {
     let input = include_str!("input.txt").trim_end();
     let cols = input.find("\n").unwrap();
     let rows = input.len() / cols;
-    Array2D::from_iter_row_major(
+    Grid::from_iter_row_major(
         input.bytes().filter_map(|b| {
             let b = b.wrapping_sub('0' as u8);
             if b < 10 {
@@ -39,10 +37,10 @@ fn parse() -> Array2D<u8> {
     )
 }
 
-fn expand(grid: &Array2D<u8>) -> Array2D<u8> {
+fn expand(grid: &Grid<u8>) -> Grid<u8> {
     let rows = grid.num_rows();
     let cols = grid.num_columns();
-    let mut out = Array2D::filled_with(0u8, rows * 5, cols * 5);
+    let mut out = Grid::filled_with(0u8, rows * 5, cols * 5);
     let add = |a, b, c| {
         let mut v = a + b + c;
         while v > 9 {
@@ -53,10 +51,10 @@ fn expand(grid: &Array2D<u8>) -> Array2D<u8> {
 
     for r in 0..rows {
         for c in 0..cols {
-            let v = grid[(r, c)];
+            let v = grid[Point(r, c)];
             for i in 0..5 {
                 for j in 0..5 {
-                    out[(r + i * rows, c + j * rows)] = add(v, i as u8, j as u8);
+                    out[Point(r + i * rows, c + j * rows)] = add(v, i as u8, j as u8);
                 }
             }
         }
@@ -64,28 +62,15 @@ fn expand(grid: &Array2D<u8>) -> Array2D<u8> {
     out
 }
 
-#[derive(Clone, Copy, Debug, Eq, PartialEq, Hash)]
+#[derive(Clone, Copy, Debug, Eq, PartialEq)]
 struct Point(usize, usize);
 
-impl<T: Clone> Index<Point> for Array2D<T> {
-    type Output = T;
-    fn index(&self, p: Point) -> &Self::Output {
-        self.index((p.0, p.1))
-    }
-}
-
-impl<T: Clone> IndexMut<Point> for Array2D<T> {
-    fn index_mut(&mut self, p: Point) -> &mut Self::Output {
-        self.index_mut((p.0, p.1))
-    }
-}
-
 struct Graph {
-    grid: Array2D<u8>,
+    grid: Grid<u8>,
 }
 
 impl Graph {
-    fn new(grid: Array2D<u8>) -> Graph {
+    fn new(grid: Grid<u8>) -> Graph {
         Graph { grid: grid }
     }
 
@@ -94,7 +79,7 @@ impl Graph {
         F: FnMut(Point, u8),
     {
         let mut cb = |r, c| {
-            f(Point(r, c), self.grid[(r, c)]);
+            f(Point(r, c), self.grid[Point(r, c)]);
         };
         if p.0 > 0 {
             cb(p.0 - 1, p.1);
@@ -112,7 +97,8 @@ impl Graph {
 
     fn shortest_path_cost(&self) -> Result<u64, Error> {
         let mut q = BinaryHeap::new();
-        let mut visited = Array2D::filled_with(false, self.grid.num_rows(), self.grid.num_columns());
+        let mut visited =
+            Grid::filled_with(false, self.grid.num_rows(), self.grid.num_columns());
 
         let start = Point(0, 0);
         let end = Point(self.grid.num_rows() - 1, self.grid.num_columns() - 1);
@@ -159,5 +145,68 @@ impl std::cmp::PartialOrd for QEntry {
 impl std::cmp::Ord for QEntry {
     fn cmp(&self, other: &Self) -> std::cmp::Ordering {
         self.cost.cmp(&other.cost).reverse()
+    }
+}
+
+struct Grid<T: Copy> {
+    rows: usize,
+    cols: usize,
+    els: Vec<T>,
+    stripe_shift: u32,
+}
+
+impl<T: Copy> Grid<T> {
+    fn filled_with(element: T, rows: usize, cols: usize) -> Grid<T> {
+        let stripe_shift = cols.next_power_of_two().trailing_zeros();
+        let els = vec![element; (1 << stripe_shift) * rows];
+        Grid {
+            rows: rows,
+            cols: cols,
+            els: els,
+            stripe_shift: stripe_shift,
+        }
+    }
+
+    fn from_iter_row_major<I>(mut iter: I, rows: usize, cols: usize) -> Grid<T>
+    where
+        I: Iterator<Item = T>,
+    {
+        let stripe_shift = cols.next_power_of_two().trailing_zeros();
+        let mut els = Vec::with_capacity((1 << stripe_shift) * rows);
+        for _ in 0..rows {
+            for _ in 0..cols {
+                els.push(iter.next().unwrap());
+            }
+            for _ in cols..(1 << stripe_shift) {
+                els.push(els[0]);
+            }
+        }
+        Grid {
+            rows: rows,
+            cols: cols,
+            els: els,
+            stripe_shift: stripe_shift,
+        }
+    }
+
+    fn num_rows(&self) -> usize {
+        self.rows
+    }
+
+    fn num_columns(&self) -> usize {
+        self.cols
+    }
+}
+
+impl<T: Copy> std::ops::Index<Point> for Grid<T> {
+    type Output = T;
+    fn index(&self, p: Point) -> &Self::Output {
+        self.els.get(p.0 << self.stripe_shift | p.1).unwrap()
+    }
+}
+
+impl<T: Copy> std::ops::IndexMut<Point> for Grid<T> {
+    fn index_mut(&mut self, p: Point) -> &mut Self::Output {
+        self.els.get_mut(p.0 << self.stripe_shift | p.1).unwrap()
     }
 }
